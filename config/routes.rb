8ec62282da5a3c3a7887cf606d7c45f22efd5ec2
Navid Mostafavi <navidm@gmail.com>
2014-08-30 11:20:49 -0700

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  Raffler.ApplicationController.routes.draw do
+    root to: 'application#index'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
